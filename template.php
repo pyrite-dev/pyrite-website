b2c389736051ba4dec1ad2eca5130a724ea810e7
ねこがくれ <osjin83@gmail.com>
2025-05-23 15:25:45 +0900

    template.phpを新規作成し、基本的な構造を追加しました。

---
+++ template.php
@@ -0,0 +1,8 @@
+<?php 
+    $title = "Title";
+    include 'include/header.php';
+?>
+<!-- CONTENT -->
+      Write Content(HTML 4.01) here
+<!-- END CONTENT -->
+<?php include 'include/footer.php'; ?>

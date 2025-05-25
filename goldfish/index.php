5a6718f4ac0435416af40e7c9d9afc34a433c2c4
NishiOwO <nishi@nishi.boats>
2025-05-25 10:37:48 +0900

    add doc

--- goldfish/index.php
+++ goldfish/index.php
@@ -7,5 +7,6 @@
 <p>GoldFish is an engine for building 3D games freely, and offering a simple architecture combined with flexibility and extensibility.</p>
 <p>The engine is designed to allow real-time placement of parts and dynamic world changes, making it ideal for learning, experimentation, and creative development.</p>
 <p>It emphasizes giving users full control over the details of their world.</p>
+<p>GoldFish documentation is available <a href="/goldfish-doc">here</a>.</p>
 <!-- END CONTENT -->
 <?php include '../include/footer.php'; ?>

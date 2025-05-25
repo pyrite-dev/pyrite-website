d66fb31172af1e024da192b12a945c6bee09d599
ねこがくれ <osjin83@gmail.com>
2025-05-25 12:45:23 +0900

    Linux Install URL

--- nishbox/index.php
+++ nishbox/index.php
@@ -13,6 +13,7 @@ NishBox is a 3D sandbox game based on <a href="/goldfish">GoldFish engine</a>.
 <ul>
     <li>Windows 32-bit: <a href="https://j.nishi.boats/job/NishBox/job/master/lastSuccessfulBuild/artifact/install32.exe">install32.exe</a></li>
     <li>Windows 64-bit: <a href="https://j.nishi.boats/job/NishBox/job/master/lastSuccessfulBuild/artifact/install64.exe">install64.exe</a></li>
+    <li>Linux 64-bit: <a href="https://j.nishi.boats/job/NishBox/job/master/lastSuccessfulBuild/artifact/nishbox64.AppImage">nishbox64.AppImage</a>
 </ul>
 <h2>Contributors</h2>
 <a href="https://github.com/pyrite-dev/nishbox/graphs/contributors">

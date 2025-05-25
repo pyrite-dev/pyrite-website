abc5b0509fd338ca50bf67a831a45a61db0da689
NishiOwO <nishi@nishi.boats>
2025-05-25 11:00:15 +0900

    fix

--- goldfish/index.php
+++ goldfish/index.php
@@ -7,7 +7,7 @@
 <h2 id="about">About</h2>
 <p>GoldFish is an engine for building 3D games freely, and offering a simple architecture combined with flexibility and extensibility.</p>
 <p>The engine is designed to allow real-time placement of parts and dynamic world changes, making it ideal for learning, experimentation, and creative development.</p>
-<p>GithubRepo is <a href="https://github.com/pyrite-dev/goldfish">here</a>.</p>
+<p>Github repository is <a href="https://github.com/pyrite-dev/goldfish">here</a>.</p>
 <p>It emphasizes giving users full control over the details of their world.</p>
 <h2>Document</h2>
 <p>GoldFish documentation is available <a href="/goldfish-doc">here</a>.</p>

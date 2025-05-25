e6442b2dc88ee3be0a5510dbbd2e4b484267f464
NishiOwO <nishi@nishi.boats>
2025-05-25 10:29:38 +0900

    a

--- nishbox/index.php
+++ nishbox/index.php
@@ -3,5 +3,8 @@
     include '../include/header.php';
 ?>
 <!-- CONTENT -->
+<p>
+NishBox is a 3D sandbox game based on <a href="/goldfish">GoldFish engine</a>.
+</p>
 <!-- END CONTENT -->
 <?php include '../include/footer.php'; ?>

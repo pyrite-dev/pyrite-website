434f215c512fa115ac0577caf8ab55d3cdd09c00
NishiOwO <nishi@nishi.boats>
2025-06-09 00:00:44 +0900

    update

--- nishbox/index.php
+++ nishbox/index.php
@@ -15,6 +15,14 @@ NishBox is a 3D sandbox game based on <a href="/goldfish">GoldFish engine</a>.
     <li>Windows 64-bit: <a href="https://j.nishi.boats/job/NishBox/job/master/lastSuccessfulBuild/artifact/install64.exe">install64.exe</a></li>
     <li>Linux 64-bit: <a href="https://j.nishi.boats/job/NishBox/job/master/lastSuccessfulBuild/artifact/nishbox64.AppImage">nishbox64.AppImage</a>
 </ul>
+<h2>Requirements (probably)</h2>
+<ul>
+	<li>
+		For Windows 32-bit, you need <i>at least</i> NT 3.51 to work, but official builds require NT 4.<br>
+		To build executable that works on NT 3.51, you need to get MSYS <b>1</b> setup.
+	</li>
+	<li>For Windows 64-bit, you need 7 (not sure about 64-bit XP).</li>
+</ul>
 <h2>Contributors</h2>
 <a href="https://github.com/pyrite-dev/nishbox/graphs/contributors">
   <img src="https://contrib.rocks/image?repo=pyrite-dev/nishbox" />
